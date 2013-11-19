0ab17f4e01801a7feceda789a1514bb3919d62f7
Gustav Lindqvist <gustav@glindqvist.se>
2013-11-19 12:05:38 +0100

    Added one class and added functions for User
    Added class for Purchase and added functions for User class

--- jaws-includes/class-user.php
+++ jaws-includes/class-user.php
@@ -1,4 +1,36 @@
 <?php 
+include '../jaws-includes/functions.php';
+include '../jaws-includes/db.php';
 
+$marcus = new User('574549586345');
+
+$marcus->Card->addCard();
+
+
+class User{
+    private $personalNr;
+    private $personalNr;
+    private $lastName;
+    private $streetAddress;
+    private $postAddress;
+    private $city;
+    private $telephone;
+    
+    public function __construct() {
+        
+    }
+    
+    public function getCards(){
+        // return a list of all cards associated with the user, if there is only one card, return that one, else return false
+    }
+    
+    public function addCard(){
+        // Add a new card associated with the user, return true if successful, else false.
+    }
+    
+    public function removeCard($cardId) {
+        // remove card associated with the user;
+    }
+}
 
 ?>

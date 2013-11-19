eeba2fc2db77d1204f8b6ea7f5a008d27c894ed6
Gustav Lindqvist <gustav@glindqvist.se>
2013-11-19 10:17:43 +0100

    Added empty functions and structure for db.php
    * Added empty functions för database handling.
    * Added config.php for executing and handling configuration of the
    system.
    * Updated UML diagram to reflect changes in file structure.
    * Added the files class-product.php and class-user.php for holding the
    product and user classes.

--- jaws-includes/db.php
+++ jaws-includes/db.php
@@ -1,4 +1,120 @@
 <?php
 
-
-?>
+class Database {
+    private $database = "";
+    
+    public function __construct($dbHost,$dbUser,$dbPassword,$dbName) {
+        $database = mysqli_connect($dbHost,$dbUser,$dbPassword,$dbName) or die("Error " . mysqli_error($database));
+    }
+    
+    /*  #################################
+        User
+    */  #################################
+    
+    public function addUser() { // Adds a user to the database.
+        return true or false;
+    }
+    
+    public function editUser() { // Edits a user and returns a boolean.
+        return true or false;
+    }
+    
+    public function deleteUser() { // Returns a user.
+        return true or false;
+    }
+    
+    public function getUser() { // Returns a user.
+        return array;
+    }
+    
+    public function getUsers() { // Returns an array of users.
+        return array;
+    }
+    
+    // Purchase
+    
+    public function getUsersPurchases() {
+        
+    }
+    
+    // Login
+    
+    public function matchPassword(string email, string password) {
+        return true or false;
+    }
+    
+    // Card
+    
+    public function addCard() { // Adds a card
+        
+    }
+    
+    public function removeCard() { // Removes a card
+        
+    }
+    
+    public function editCard() { // May not be needed, cards never get changed.
+        
+    }
+    
+    /*  #################################
+        Purchase
+    */  #################################
+    
+    public function addPurchase($personalNr,$discount,$chargedCard,$purchaseList) { // Adds a purchase to the database.
+        
+        $time = getUnixTime(); // Get unixtime
+        // ADD PURCHASE TO TABLES
+        addPurchaseList($purchaseList,$purchaseId); // Call function for adding a purchaseList into the appropriate table.
+        return true/false;
+        
+    }
+    
+    private function addPurchaseList($purchaseList,$purchaseId) { 
+        // LOOP THROUGH ARRAY AND ADD EVERY ROW INTO TABLE
+    }
+    
+    public function editPurchase($purchaseId) { // Edit a purchase
+        return true/false;
+    }
+    
+    public function removePurchase($purchaseId) { // Removes a purchase and associated purchased items (purchaseList)
+        return true/false;
+    }
+    
+    public function getPurchase($purchaseId) { // Returns a purchase
+        return array;
+    }
+    
+    /*  #################################
+        Product
+    */  #################################
+    
+    public function searchProducts($query){ //Return an array of items matching query
+        return array;
+    }
+    
+    public function addProduct() { // Adds a product to the database.
+        return True/False;
+    }
+    
+    public function editProduct() { // Edits a product and returns a boolean.
+        return True/False;
+    }
+    
+    public function deleteProduct() { // Returns a product.
+        return True;
+    }
+    
+    public function getProduct() { // Returns a product.
+        return array;
+    }
+    
+    public function getProducts() { // Returns an array of products.
+        return array;
+    }
+    
+    /*  #################################
+        Category/Taxanomies TO BE DETERMINED
+    */  #################################     
+}

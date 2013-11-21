a9c3a9bce4f2fa42e37c70b516076c7bc76cb104
Marcusandreas <marcusandreas@hotmail.com>
2013-11-21 15:01:15 +0100

    Lots of changes to db.php
    Most functions are working, dbGetUser is not working amongs the other
    "get"-functions, need to handle the mysqli_result coming out.

--- jaws-includes/db.php
+++ jaws-includes/db.php
@@ -1,56 +1,66 @@
 <?php
 
 class Database {
-    private $database = "";
-    
+    private $database;
+
     public function __construct($dbHost,$dbUser,$dbPassword,$dbName) {
-        $database = mysqli_connect($dbHost,$dbUser,$dbPassword,$dbName) or die("Error " . mysqli_error($database));
+        $this->database = mysqli_connect($dbHost,$dbUser,$dbPassword,$dbName) or die("Error " . mysqli_error($this->database));
+        echo "Connect attempt!";
     }
     
     /*  #################################
         User
     */  #################################
-    
-    public function addUser($SSNr,$Mail,$FirstName,$LastName,$StreetAddress,$PostAddress,$City,$Telephone) { // Adds a user to the database.
-        if (mysqli_query($database, "INSERT INTO users SET SSNr='"$SSNr"',Mail='"$Mail"',FirstName='"$FirstName"',LastName='"$LastName"',StreetAddress='"$StreetAddress"',PostAddress='"$PostAddress"',City='"$City"',Telephone='"$Telephone"'") === TRUE) {
+
+    public function dbAddUser($SSNr,$Mail,$Password,$FirstName,$LastName,$StreetAddress,$PostAddress,$City,$Telephone) { // Adds a user to the database.
+        if (mysqli_query($this->database, "INSERT INTO users (SSNr,Mail,Password,FirstName,LastName,StreetAddress,PostAddress,City,Telephone) VALUES ('$SSNr','$Mail','$Password','$FirstName','$LastName','$StreetAddress','$PostAddress','$City','$Telephone')") === TRUE) {
+            echo "success addUser\n";
             return true;
         }else{
+            echo "failure addUser\n";
+            return false;
+        }
+    }
+    public function dbEditUser($SSNr,$ChangedRow,$ChangeRowValue) { // Edits a user and returns a boolean.
+        if(mysqli_query($this->database, "UPDATE users SET $ChangedRow='$ChangeRowValue' WHERE SSNr=$SSNr")===TRUE){
+            echo "success editUser\n";
+            return true;
+        }else{
+            echo "failure editUser\n";
             return false;
         }
     }
     
-    public function editUser($ChangedRow,$ChangeRowValue,$SSNr) { // Edits a user and returns a boolean.
-        if(mysqli_query($database, "UPDATE users SET "$ChangedRow"='"$ChangeRowValue"' WHERE SSNr='"$SSNr"'")===TRUE){
+    public function dbDeleteUser($SSNr) { // Returns true if success,false if failed
+        if(mysqli_query($this->database, "DELETE FROM users WHERE SSNr='$SSNr'")===TRUE){
+            echo "success deleteUser\n";
             return true;
         }else{
+            echo "FAILURE deleteUser\n";
             return false;
         }
     }
     
-    public function deleteUser($SSNr) { // Returns true if success,false if failed
-        if(mysqli_query($database, "DELETE FROM users WHERE SSNr='"$SSNr"'")===TRUE){
-            return true;
+    public function dbGetUser($SSNr) { // Returns a user.
+        $result=mysqli_query($this->database, "SELECT * FROM users WHERE SSNr='$SSNr'");
+        if($result===TRUE){
+            return $User;
         }else{
             return false;
         }
-    }
-    
-    public function getUser($SSNr) { // Returns a user.
-        $User=mysqli_query($database, "SELECT * FROM users WHERE SSNr='"$SSNr"'");
-        return $User;
     }
     // Purchase
     
-    public function getUsersPurchases($SSNr) {
-        $UserPurchases=mysqli_query($database, "SELECT * FROM purchases WHERE SSNr='"$SSNr"'");
+    public function dbGetUsersPurchases($SSNr) {
+        $UserPurchases=mysqli_query($this->database, "SELECT * FROM purchases WHERE SSNr='$SSNr'");
         return $UserPurchases;
     }
     
     // Login
     
-    public function matchPassword($LoginEmail, $LoginPassword) {
-        if(mysqli_query($database, "SELECT Mail FROM users WHERE Mail='"$LoginEmail"'")!=FALSE){
-            $password=mysqli_query($database, "SELECT Password FROM users WHERE Mail='"$LoginEmail"'");
+    public function dbMatchPassword($LoginEmail, $LoginPassword) {
+        if(mysqli_query($this->database, "SELECT Mail FROM users WHERE Mail='$LoginEmail'")!=FALSE){
+            $password=mysqli_query($this->database, "SELECT Password FROM users WHERE Mail='$LoginEmail'");
             if($password==$LoginPassword){
                 return true;
             }
@@ -61,16 +71,16 @@ class Database {
     
     // Card
     
-    public function addCard($CardId,$CardNr,$CardName,$ExpiryMonth,$ExpiryYear) { // Adds a card
-         if (mysqli_query($database, "INSERT INTO cards SET CardId='"$CardId"',CardNr='"$CardNr"',CardName='"$CardName"',ExpiryMonth='"$ExpiryMonth"', ExpiryYear='"$ExpiryYear"'") === TRUE) {
+    public function dbAddCard($CardId,$CardNr,$CardName,$ExpiryMonth,$ExpiryYear) { // Adds a card
+         if (mysqli_query($this->database, "INSERT INTO cards SET CardId='$CardId',CardNr='$CardNr',CardName='$CardName',ExpiryMonth='$ExpiryMonth', ExpiryYear='$ExpiryYear'") === TRUE) {
              return true;
          }else{
             return false;
         }
     }
     
-    public function removeCard($CardId) { // Removes a card
-        if(mysqli_query($database, "DELETE FROM cards WHERE CardNr='"$CardId"'")===TRUE){
+    public function dbRemoveCard($CardId) { // Removes a card
+        if(mysqli_query($this->database, "DELETE FROM cards WHERE CardNr='$CardId'")===TRUE){
             return true;
         }else{
             return false;
@@ -82,9 +92,9 @@ class Database {
         Purchase
     */  #################################
     
-    public function addPurchase($SSNr,$Discount,$ChargedCard,$purchaseList) { // Adds a purchase to the database.
+    public function dbAddPurchase($SSNr,$Discount,$ChargedCard,$purchaseList) { // Adds a purchase to the database.
         $time = getUnixTime(); // Get unixtime
-        if(mysqli_query($database, "INSERT INTO purchases SET SSNr='"$SSNr"' PurchaseDate='"$time"',Discount='"$Discount"',ChargedCard='"$ChargedCard"'")===TRUE){
+        if(mysqli_query($this->database, "INSERT INTO purchases SET SSNr='$SSNr' PurchaseDate='$time',Discount='$Discount',ChargedCard='$ChargedCard'")===TRUE){
             // ADD PURCHASE TO TABLES
             addPurchaseList($purchaseList,$purchaseId); // Call function for adding a purchaseList into the appropriate table.
             return true;
@@ -93,64 +103,64 @@ class Database {
         }
     }
     
-    private function addPurchaseList($purchaseList,$purchaseId) { 
+    private function dbAddPurchaseList($purchaseList,$purchaseId) {
         // LOOP THROUGH ARRAY AND ADD EVERY ROW INTO TABLE
     }
     
-    public function editPurchase($purchaseId) { // Edit a purchase
-        return true/false;
+    public function dbEditPurchase($purchaseId) { // Edit a purchase
+        return true;
     }
     
-    public function removePurchase($PurchaseId) { // Removes a purchase and associated purchased items (purchaseList)
-        if(mysqli_query($database, "DELETE FROM purchases WHERE PurchaseId='"$PurchaseId"'")===TRUE && mysqli_query($database, "DELETE FROM purchase_list WHERE PurchaseId='"$PurchaseId"'")===TRUE){
+    public function dbRemovePurchase($PurchaseId) { // Removes a purchase and associated purchased items (purchaseList)
+        if(mysqli_query($this->database, "DELETE FROM purchases WHERE PurchaseId='$PurchaseId'")===TRUE && mysqli_query($database, "DELETE FROM purchase_list WHERE PurchaseId='$PurchaseId'")===TRUE){
             return true;
         }else{
             return false;
         }
     }
     
-    public function getPurchase($purchaseId) { // Returns a purchase
-        return array;
+    public function dbGetPurchase($purchaseId) { // Returns a purchase
+        return true;
     }
     
     /*  #################################
         Product
     */  #################################
     
-    public function searchProducts($query){ //Return an array of items matching query
-        return array;
+    public function dbSearchProducts($query){ //Return an array of items matching query
+        return true;
     }
     
-    public function addProduct($Name,$Category,$Price,$Stock) { // Adds a product to the database.
-        if(mysqli_query($database, "INSERT INTO products SET Name='"$Name"',Category='"$Category"',Price='"$Price"',Stock='"$Stock"'")===TRUE){
+    public function dbAddProduct($Name,$Category,$Price,$Stock) { // Adds a product to the database.
+        if(mysqli_query($this->database, "INSERT INTO products SET Name='$Name',Category='$Category',Price='$Price',Stock='$Stock'")===TRUE){
             return true;
         }else{
             return false;
         }
     }
     
-    public function editProduct($ProductId) { // Edits a product and returns a boolean.
-        if(mysqli_query($database, "UPDATE products SET "$ChangedRow"='"$ChangeRowValue"' WHERE ProductId='"$ProductId"'")===TRUE){
+    public function dbEditProduct($ProductId,$ChangedRow,$ChangeRowValue) { // Edits a product and returns a boolean.
+        if(mysqli_query($this->database, "UPDATE products SET $ChangedRow='$ChangeRowValue' WHERE ProductId='$ProductId'")===TRUE){
             return true;
         }else{
             return false;
         }
     }
     
-    public function deleteProduct($ProductId) { // Removes a product from table
-        if(mysqli_query($database, "DELETE FROM products WHERE ProductId='"$ProductId"'")===TRUE){
+    public function dbDeleteProduct($ProductId) { // Removes a product from table
+        if(mysqli_query($this->database, "DELETE FROM products WHERE ProductId='$ProductId'")===TRUE){
             return true;
         }else{
             return false;
-    }
-    }
-    
-    public function getProduct() { // Returns a product.
-        return array;
+        }
     }
     
-    public function getProducts() { // Returns an array of products.
-        return array;
+    public function dbGetProduct() { // Returns a product.
+        return true;
+    }
+    
+    public function dbGetProducts() { // Returns an array of products.
+        return true;
     }
     
     /*  #################################
--- jaws-includes/dummy.php
+++ jaws-includes/dummy.php
@@ -1,16 +1,11 @@
-<html>
-    <head>
+<?php
+    include 'db.php';
 
-    </head>
-    <body>
-        <?php
-            include db.php;
-            $dbHost = "localhost";
-            $dbUser = "root";
-            $dbPassword = "621955";
-            $dbName = "jaws_db";
+    $dbHost = "localhost";
+    $dbUser = "root";
+    $dbPassword = "621955";
+    $dbName = "jaws_db";
 
-            $db = new Database($dbHost,$dbUser,$dbPassword,$dbName);
-        ?>
-    </body>
-</html>
+    $db = new Database($dbHost,$dbUser,$dbPassword,$dbName);
+    $db->getUser(199205075931);
+?>

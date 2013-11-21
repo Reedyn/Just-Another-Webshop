691e584be3fac77ad03139072236be0f515cf646
Gustav Lindqvist <gustav@glindqvist.se>
2013-11-21 14:28:48 +0100

    Added functions in User and Product classes
    Added functions for everything except categories for User, Admin and
    Product classes

--- jaws-includes/class-product.php
+++ jaws-includes/class-product.php
@@ -1,4 +1,76 @@
 <?php 
 
+class Product{
+    private $productId;
+    private $name;
+    private $price;
+    private $stock;
+    private $imageUrl;
+    private $category;
+
+    public function __construct($productId, $name, $price, $stock, $imageUrl, $category) {
+        $this->productId    = $productId;
+        $this->name         = $name;
+        $this->price        = $price;
+        $this->stock        = $stock;
+        $this->imageUrl     = $imageUrl;
+        $this->category     = $category;
+    }
+    public function getProductId(){
+        return $this->productId;
+    }
+    
+    public function setProductId($productId){
+        $this->productId = $productId;
+    }
+    
+    public function getName(){
+        return $this->name;
+    }
+    
+    public function setName($name){
+        $this->name = $name;
+    }
+    
+    public function getPrice(){
+        return $this->name;
+    }
+    
+    public function setPrice($price){
+        $this->price = $price;
+    }
+    
+    public function inStock(){
+        if ($this->stock > 0){
+            return true;
+        } else {
+            false;
+        }
+    }
+    
+    public function getStock(){
+        return $this->name;
+    }
+    
+    public function setStock($stock){
+        $this->stock = $stock;
+    }
+    
+    public function getImage(){
+        return $this->imageUrl;
+    }
+    
+    public function setImage($imageUrl){
+        $this->$imageUrl = $imageUrl;
+    }
+    
+    public function getCategory(){
+        
+    }
+    
+    public function setCategory(){
+        
+    }
+}
 
 ?>
--- jaws-includes/class-user.php
+++ jaws-includes/class-user.php
@@ -2,35 +2,115 @@
 include '../jaws-includes/functions.php';
 include '../jaws-includes/db.php';
 
-$marcus = new User('574549586345');
+function getUser($personalNr) { // Returns a user from the database as a User class.
+    $userData = loadUser();
+    $user = new User($userData['personalNr'], $userData['name'], $userData['lastName'], $userData['streetAddress'], $userData['postAddress'], $userData['city'], $userData['phone']);
+    return $user;
+}
 
-$marcus->Card->addCard();
 
 
 class User{
     private $personalNr;
-    private $personalNr;
+    private $name;
     private $lastName;
     private $streetAddress;
     private $postAddress;
     private $city;
-    private $telephone;
-    
-    public function __construct() {
-        
+    private $phone;
+
+    public function __construct($personalNr, $name, $lastName, $streetAddress, $postAddress, $city, $phone) {
+        $this->personalNr    = $personalNr;
+        $this->name          = $name;
+        $this->lastName      = $lastName;
+        $this->streetAddress = $streetAddress;
+        $this->postAddress   = $postAddress;
+        $this->city          = $city;
+        $this->phone         = $phone;
+    }
+    public function getPersonalNr(){
+        return $this->personalNr;
     }
     
-    public function getCards(){
+    public function setPersonalNr($personalNr){
+        $this->personalNr = $personalNr;
+    }
+    
+    public function getName(){
+        return $this->name;
+    }
+    
+    public function setName($name){
+         $this->name = $name;
+    }
+    
+    public function getLastName(){
+        return $this->lastName;
+    }
+    
+    public function setLastName($lastName){
+         $this->lastName = $lastName;
+    }
+    
+    public function getFullName(){
+        return $this->name . " " . $this->lastName;
+    }
+    
+    public function getStreetAddress(){
+        return $this->streetAddress;
+    }
+    
+    public function setStreetAddress($streetAddress){
+         $this->streetAddress = $streetAddress;
+    }
+    
+    public function getPostAddress(){
+        return $this->postAddress;
+    }
+    
+    public function setPostAddress($postAddress){
+         $this->postAddress = $postAddress;
+    }
+    
+    public function getCity(){
+        return $this->city;
+    }
+    
+    public function setCity($city){
+         $this->city = $city;
+    }
+    
+    public function getPhone(){
+        return $this->phone;
+    }
+    
+    public function setPhone($phone){
+         $this->phone = $phone;
+    }
+
+    public function getCard(){
         // return a list of all cards associated with the user, if there is only one card, return that one, else return false
     }
-    
+
     public function addCard(){
         // Add a new card associated with the user, return true if successful, else false.
     }
-    
-    public function removeCard($cardId) {
+
+    public function removeCard() {
         // remove card associated with the user;
     }
+
+    public function saveUser() {
+    }
 }
+
+class Admin extends User {
+    private $isAdmin = true;
+    
+    public function isAdmin(){
+        return $this->isAdmin;
+    }
+}
+
 
 ?>

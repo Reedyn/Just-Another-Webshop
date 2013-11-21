09885083813d1c64391e586c9f7d53e95e9fc961
Marcusandreas <marcusandreas@hotmail.com>
2013-11-21 21:56:47 +0100

    Updated SQL-dump and db.php
    Purchase was changed to Order

--- jaws-includes/db.php
+++ jaws-includes/db.php
@@ -1,6 +1,6 @@
 <?php
 
-class Database {
+class Database extends mysqli {
     protected $database;
     public function __construct($dbHost,$dbUser,$dbPassword,$dbName) {
         $this->database = mysqli_connect($dbHost,$dbUser,$dbPassword,$dbName) or die("Error " . mysqli_error($this->database));
@@ -45,7 +45,7 @@ class Database {
         $row = mysqli_fetch_assoc($result);
         if (!$row)
         {
-            echo 'Error - user does not exist';
+            echo 'Error - User does not exist';
             exit();
             return false;
         }else{
@@ -53,14 +53,14 @@ class Database {
             return $row;
         }
     }
-    // Purchase
+    // Orders
     
-    public function dbGetUsersPurchases($SSNr) {
-        $result=mysqli_query($this->database, "SELECT * FROM purchases WHERE SSNr='$SSNr'");
+    public function dbGetUsersOrders($SSNr) {
+        $result=mysqli_query($this->database, "SELECT * FROM orders WHERE SSNr='$SSNr'");
         $row = mysqli_fetch_assoc($result);
         if (!$row)
         {
-            echo 'Error - No purchases exist for this user';
+            echo 'Error - No order exist for this user';
             exit();
             return false;
         }else{
@@ -72,7 +72,7 @@ class Database {
     // Login
     
     public function dbMatchPassword($LoginEmail, $LoginPassword) {
-        //NOT DONE-----------------------------------------------------*
+        //NOT DONE----------------------------------------------------------------------------------------------------*
         if(mysqli_query($this->database, "SELECT Mail FROM users WHERE Mail='$LoginEmail'")!=FALSE){
             $password=mysqli_query($this->database, "SELECT Password FROM users WHERE Mail='$LoginEmail'");
             if($password==$LoginPassword){
@@ -103,56 +103,56 @@ class Database {
 
     
     /*  #################################
-        Purchase
+        Order
     */  #################################
 
-    private function dbAddPurchaseList() {
+    private function dbAddOrderList() {
         //LOOP THROUGH ARRAY AND ADD EVERY ROW INTO TABLE
         return true;
 
     }
 
-    public function dbAddPurchase($SSNr,$Discount,$ChargedCard,$purchaseList) { // Adds a purchase to the database.
+    public function dbAddOrder($SSNr,$Discount,$ChargedCard,$orderList) { // Adds a order to the database.
         //NOT DONE ----------------------------------------------------------------------------------------------------*
         $time = getUnixTime(); // Get unixtime
-        if(mysqli_query($this->database, "INSERT INTO purchases SET SSNr='$SSNr' PurchaseDate='$time',Discount='$Discount',ChargedCard='$ChargedCard'")===TRUE){
-            // ADD PURCHASE TO TABLES
-            dbAddPurchaseList($purchaseList,$purchaseId); // Call function for adding a purchaseList into the appropriate table.
+        if(mysqli_query($this->database, "INSERT INTO orders SET SSNr='$SSNr' OrderDate='$time',Discount='$Discount',ChargedCard='$ChargedCard'")===TRUE){
+            // ADD ORDER TO TABLES
+            dbAddOrderList($OrderList,$OrderId); // Call function for adding a OrderList into the appropriate table.
             return true;
         }else{
             return false;
         }
     }
     
-    public function dbEditPurchase($PurchaseId,$ChangedRow,$ChangeRowValue) { // Edit a purchase
-        if(mysqli_query($this->database, "UPDATE purchases SET $ChangedRow='$ChangeRowValue' WHERE PurchaseId='$PurchaseId'")===TRUE){
+    public function dbEditOrder($OrderId,$ChangedRow,$ChangeRowValue) { // Edit an order
+        if(mysqli_query($this->database, "UPDATE orders SET $ChangedRow='$ChangeRowValue' WHERE OrderId='$OrderId'")===TRUE){
             return true;
         }else{
             return false;
         }
     }
     
-    public function dbRemovePurchase($PurchaseId) { // Removes a purchase and associated purchased items (purchaseList)
-        if(mysqli_query($this->database, "DELETE FROM purchases WHERE PurchaseId='$PurchaseId'")===TRUE){
-            if(mysqli_query($this->database, "DELETE FROM purchase_list WHERE PurchaseId='$PurchaseId'")===TRUE){
+    public function dbRemoveOrder($OrderId) { // Removes a order and associated ordered items (orderList)
+        if(mysqli_query($this->database, "DELETE FROM orders WHERE OrderId='$OrderId'")===TRUE){
+            if(mysqli_query($this->database, "DELETE FROM order_list WHERE OrdersId='$OrderId'")===TRUE){
                 return true;
             }else{
-                echo "deletes from purchase_list failed";
+                echo "deletes from order_list failed";
                 return false;
             }
         }
         else{
-            echo "deletes from purchases failed";
+            echo "deletes from orders failed";
             return false;
         }
     }
     
-    public function dbGetPurchase($PurchaseId) { // Returns a purchase
-        $result=mysqli_query($this->database, "SELECT * FROM purchases WHERE PurchaseId='$PurchaseId'");
+    public function dbGetOrders($OrderId) { // Returns a order
+        $result=mysqli_query($this->database, "SELECT * FROM orders WHERE OrderId='$OrderId'");
         $row = mysqli_fetch_assoc($result);
         if (!$row)
         {
-            echo 'Error - purchase does not exist';
+            echo 'Error - order does not exist';
             exit();
             return false;
         }else{
@@ -167,6 +167,17 @@ class Database {
     */  #################################
     
     public function dbSearchProducts($SearchQuery){ //Return an array of items matching query
+        $result=mysqli_query($this->database, "SELECT * FROM products WHERE Name,Category LIKE '%$SearchQuery%'");
+        $row = mysqli_fetch_assoc($result);
+        if (!$row)
+        {
+            echo 'Error - Couldnt find anything';
+            exit();
+            return false;
+        }else{
+            var_dump($row);
+            return $row;
+        }
         return true;
     }
     

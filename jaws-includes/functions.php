6390b4dfd6f50fc4a50392edd49938552fa2dbd9
Marcusandreas <marcusandreas@hotmail.com>
2013-11-26 18:32:22 +0100

    asd
    asd

---
+++ jaws-includes/functions.php
@@ -0,0 +1,11 @@
+<?php
+    //Includes
+    include 'db.php';
+    include 'config.php';
+
+    //Initialize the class Database
+    $db=new Database($dbHost,$dbUser,$dbPassword,$dbName);
+
+    //Functions
+
+?>

0bcae59140a1e1536ffd2153928f31bbb241ded4
Gustav Lindqvist <gustav@glindqvist.se>
2013-12-06 11:56:58 +0100

    Added basic functionality to Index
    * Added basic functionality to Index and tested in index-dummy.
    * Added various template pages.

--- jaws-includes/class-index.php
+++ jaws-includes/class-index.php
@@ -1,10 +1,23 @@
 <?php
 
+
 class Index {
+	protected $theme;
+	public function __construct($theme = "default"){
+		$this->theme = $theme;
+	}
 	
+	public function initialize() { 		// Primary function
+		if(isset($_GET['home'])){ 	// If no variable is set.
+			echo "Home";
+		} elseif (isset($_GET['page'])) {
+			echo "Page";
+		} else {
+			echo "404";
+		}
+	}
 	
-	public function __construct(){
-		
+	public function getTitle(){
 	}
 }
 

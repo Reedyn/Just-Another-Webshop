15916297b7355d74022ca64c435ec8c853d24bba
Marcusandreas <marcusandreas@hotmail.com>
2013-12-10 22:36:05 +0100

    Fixed merging errors in functions.php

--- jaws-includes/functions.php
+++ jaws-includes/functions.php
@@ -1,38 +1,5 @@
 <?php
-<<<<<<< HEAD
-public function listProducts($category,$listType){ // List products in the fashion specified.
-<<<<<<< HEAD
-    // Get a list of products from database and save the array in $products
-    //Loop through array and add all products to $products as a Product Class
-    
-    $products[];
-=======
-    // Get a list of products from database and save the array in $products[]
-    var $products[];
->>>>>>> develop
-    
-    if ($listType == 'list') {
-        echo '<ul class="product-list">';
-        for ($i = 0; i < $products.length(); i++){
-            echo '  <li>' . '<span class="name">' . $products[i]->Name . '</span>' . '<span class="price">' . $products[i]->price . '</span>' . '<span class="stock">' . $products[i]->stock . '</span>' . '</li>'
-        }
-        echo '</ul><!-- .product-list -->'
-    } elseif ($listType == 'thumbnail') {
-        for ($i = 0; i < $products.length(); i++)
-            echo '<article class="product">';
-            echo '  <img src="'.$products[i]->imageUrl.'" class="product-image"/>';
-            echo '  <div class="product-meta">';
-            echo '  <h2 class="product-title">'. $product[i]->Name .'</h2>';
-            echo '  <span class="product-price">'. $products[i]->price .'</span>';
-            echo '  <div class="product-add-to-cart-button"></div>';
-            echo '</div><!-- .product-meta -->';
-            echo '</article>';
-    } else {
-        echo '<span class="error">No products found.</span>'
-<<<<<<< HEAD
-    } 
-}
-=======
+
     include 'class-product.php';
     include 'class-order.php';
     include 'class-user.php';
@@ -100,7 +67,6 @@ public function listProducts($category,$listType){ // List products in the fashi
         }
 
     }
->>>>>>> origin/data-functions
 
     function listOrders($listType){
         $numargs=func_num_args();
@@ -176,11 +142,4 @@ public function listProducts($category,$listType){ // List products in the fashi
             echo '<span class="error">No users found.</span>';
         }
     }
-<<<<<<< HEAD
-=======
-    }    
-}
->>>>>>> develop
-=======
->>>>>>> origin/data-functions
 ?>

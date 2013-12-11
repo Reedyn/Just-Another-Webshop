57bca35be54d096fd497f0a721b9591e2738510d
Gustav Lindqvist <gustav@glindqvist.se>
2013-12-11 19:05:42 +0100

    Mnor fixelixes

---
+++ jaws-content/navUser.php
@@ -0,0 +1,20 @@
+<nav class="site-nav">
+    <ul>
+        <li>
+            <a href="/products/1-consoles">CONSOLES</a>
+			<ul>
+				<li><a href="/products/4-xbox">XBOX</a></li>
+			</ul>
+        </li> 
+        <li><a href="/products/2-games/">GAMES</a></li>
+        <li><a href="/products/3-accessories/">ACCESSORIES</a></li>
+        <li><a href="/login/">Login/Register</a></li>
+        <li>
+        	<a href="/settings/">Settings</a>
+        	<ul>
+				<li><a href="/settings/orders">Orders</a></li>
+				<li><a href="/settings/user">User</a></li>
+			</ul>
+        </li>
+    </ul>
+</nav>
--- jaws-includes/functions.php
+++ jaws-includes/functions.php
@@ -212,7 +212,7 @@
     }
     function UserLogin(){
         global $db;
-        if($CurrentSSNr=$db->dbMatchPassword($_POST['email'],$_POST['password'])){
+        if($CurrentSSNr=$db->dbMatchPassword($_POST['email'],$_POST['password'])==TRUE){
         //if($CurrentSSNr=$db->dbMatchPassword($a,$b)==TRUE){
             $chars=array('1','2','3','4','5','6','7','8','9','0','a','b','c','d','e','f');
             $sessionkey="";
@@ -221,7 +221,7 @@
             }
             if($db->dbEditUser($CurrentSSNr,"SessionKey",$sessionkey)==TRUE){
                 echo '<span class="login_success">Login successful</span>';
-                $SESSION['SessionKey']=$sessionkey;
+                $_SESSION['SessionKey']=$sessionkey;
             }
 
         }else{

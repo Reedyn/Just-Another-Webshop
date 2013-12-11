1d8966ea2cb6bd717f431bb63613691fadfc5eeb
Judge555 <davidklaar@gmail.com>
2013-12-11 15:06:00 +0100

    head.php included

--- Temporary Project Files/Templates/page-products.php
+++ Temporary Project Files/Templates/page-products.php
@@ -1,20 +1,9 @@
-<!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-   <head>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-<title>Product Page</title>
-<meta name="description" content="">
-<meta name="viewport" content="width=device-width">
-
-<link rel="stylesheet" href="css/normalize.min.css">
-<link rel="stylesheet" href="css/style.css">
-
-<script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
-<script defer src="js/main.js"></script>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<?php include 'head.php'; ?>
+<title>Products</title>
 </head>
 <body>
 <section class="wrapper">
--- Temporary Project Files/Templates/page-settings-user.php
+++ Temporary Project Files/Templates/page-settings-user.php
@@ -2,16 +2,8 @@
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title>Product Page</title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width">
-    <link rel="stylesheet" href="css/normalize.min.css">
-    <link rel="stylesheet" href="css/style.css">
-    <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
-    <script defer src="js/main.js"></script>
-
+<?php include 'head.php'; ?>
+<title>Settings User</title>
 </head>
 <body>
 

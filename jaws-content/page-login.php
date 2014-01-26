68ce71f34e50083602ecaa8251de95a974995048
Gustav Lindqvist <gustav@glindqvist.se>
2014-01-26 21:16:11 +0100

    added redirection after logging in

--- jaws-content/page-login.php
+++ jaws-content/page-login.php
@@ -1,3 +1,4 @@
-<?php $_SESSION['logged-in'] = true; jaws_header(); ?>
+<?php $_SESSION['logged-in'] = true; jaws_header(); header("Location: /"); /* Redirect browser */
+exit(); ?>
 
 <?php jaws_footer(); ?>

49c8026d1d3dcc5a1b64a138fe44a0a368f7a82f
Gustav Lindqvist <gustav@glindqvist.se>
2014-01-31 14:26:52 +0100

    you can now save your own profile (with new password or not) via settings

--- jaws-content/page-settings-user.php
+++ jaws-content/page-settings-user.php
@@ -4,14 +4,24 @@ if(isset($_POST['user-submit'])) {
         isset($_POST['user-mail']) && preg_match("$[a-z0-9åäöÅÄÖ._%+-]+[a-zåäöÅÄÖ0-9]+@[a-z0-9.-]+\.[a-z]{2,4}$", $_POST['user-mail']) &&
         isset($_POST['user-first-name']) && preg_match("$\w+$", $_POST['user-first-name']) &&
         isset($_POST['user-last-name']) && preg_match("$\w+$", $_POST['user-last-name']) &&
-        isset($_POST['user-phone']) && preg_match("$(46|\+46|0)(-?\s?[0-9]+)+$", $_POST['user-phone'])) {
+        isset($_POST['user-phone']) && preg_match("$(46|\+46|0)(-?\s?[0-9]+)+$", $_POST['user-phone']) && 
+        isset($_POST['user-street-address']) && 
+        isset($_POST['user-post-address']) && 
+        isset($_POST['user-city'])) {
         
         // Check if user is trying to save a new password
         if (isset($_POST['user-new-password']) && preg_match("$[a-zA-ZåäöÅÄÖ0-9]{6,30}$", $_POST['user-new-password']) &&
             isset($_POST['user-old-password']) && preg_match("$[a-zA-ZåäöÅÄÖ0-9]{6,30}$", $_POST['user-new-password'])){
             
-            if (true) { //Match old password with database
-                if(true) {// Save user with new password to db
+            if ($db->dbMatchPassword($_POST['user-mail'], $_POST['user-old-password'])) { //Match old password with database
+                if($db->dbEditUser($_POST['user-ssn'],"Mail",$_POST['user-mail'],
+                    "FirstName",$_POST['user-first-name'],
+                    "LastName",$_POST['user-last-name'],
+                    "Telephone",$_POST['user-phone'],
+                    "StreetAddress",$_POST['user-street-address'],
+                    "PostAddress",$_POST['user-post-address'],
+                    "City",$_POST['user-city'],
+                    "Password",$_POST['user-new-password'])) { // Save user with new password to db
                     registerError("Your profile with your new password has been saved.","success");
                     redirect();
                 } else {
@@ -23,7 +33,13 @@ if(isset($_POST['user-submit'])) {
                 redirect();
             }
         } else { // When user didn't change password
-            if (true) { // Save user to database, 
+            if ($db->dbEditUser($_POST['user-ssn'],"Mail",$_POST['user-mail'],
+                    "FirstName",$_POST['user-first-name'],
+                    "LastName",$_POST['user-last-name'],
+                    "Telephone",$_POST['user-phone'],
+                    "StreetAddress",$_POST['user-street-address'],
+                    "PostAddress",$_POST['user-post-address'],
+                    "City",$_POST['user-city'])) { // Save user to database, 
                 registerError('Your profile has been saved','success');
                 redirect(); 
             } else {

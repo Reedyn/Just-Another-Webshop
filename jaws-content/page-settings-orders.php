67dc2c869bdf9697e1732121b3f251305f2a82be
Gustav Lindqvist <gustav@glindqvist.se>
2014-01-31 14:42:13 +0100

    Updated user settings for orders to use the new table layout

--- jaws-content/page-settings-orders.php
+++ jaws-content/page-settings-orders.php
@@ -1,5 +1,13 @@
 <?php jaws_header(); ?>
+<<<<<<< HEAD
 <?php
     listUsersOrders();
 ?>
+=======
+
+<div class="alert alert-danger">    <a class="close" data-dismiss="alert">×</a>    <strong>Denna ska ändras till att bara visa användarens <ordrar></ordrar></strong>.</div>
+
+<?php listAdminOrders(); ?>
+
+>>>>>>> Updated user settings for orders to use the new table layout
 <?php jaws_footer(); ?>

65e08a5f8e3acf941f1f3f26680f49924e93246c
Gustav Lindqvist <gustav@glindqvist.se>
2014-02-05 08:32:10 +0100

    Updated SQL-dump

--- jaws_db.sql
+++ jaws_db.sql
@@ -1,13 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.4
+-- version 3.4.10.1deb1
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Feb 01, 2014 at 03:22 PM
--- Server version: 5.6.12-log
--- PHP Version: 5.4.12
+-- Host: 10.101.1.89
+-- Generation Time: Feb 05, 2014 at 08:31 AM
+-- Server version: 5.5.30
+-- PHP Version: 5.3.10-1ubuntu3.4
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
@@ -17,10 +17,8 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `jaws_db`
+-- Database: `174637-justanotherwebshop`
 --
-CREATE DATABASE IF NOT EXISTS `jaws_db` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `jaws_db`;
 
 -- --------------------------------------------------------
 
@@ -36,14 +34,19 @@ CREATE TABLE IF NOT EXISTS `cards` (
   `ExpiryYear` char(11) NOT NULL,
   PRIMARY KEY (`CardId`),
   UNIQUE KEY `CardId` (`CardId`,`CardNr`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=36 ;
 
 --
 -- Dumping data for table `cards`
 --
 
 INSERT INTO `cards` (`CardId`, `CardNr`, `CardName`, `ExpiryMonth`, `ExpiryYear`) VALUES
-(1, 123456789, 'Marcus Börjesson', '04', '16');
+(1, 123456789, 'Marcus Börjesson', '04', '16'),
+(31, 2147483647, '54543', '15', '09'),
+(32, 2147483647, '543543', '543', '543'),
+(33, 2147483647, 'Gustav Lindqvist', '15', '09'),
+(34, 2147483647, 'Gustav Lindqvist', '16', '08'),
+(35, 2147483647, 'Gustav Lindqvist', '16', '07');
 
 -- --------------------------------------------------------
 
@@ -58,16 +61,18 @@ CREATE TABLE IF NOT EXISTS `currencies` (
   `CurrencySign` text NOT NULL,
   `CurrencyLayout` text NOT NULL,
   PRIMARY KEY (`CurrencyId`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
 
 --
 -- Dumping data for table `currencies`
 --
 
 INSERT INTO `currencies` (`CurrencyId`, `CurrencyName`, `CurrencyMultiplier`, `CurrencySign`, `CurrencyLayout`) VALUES
-(1, 'Euro', 1, '€', 'prefix'),
+(1, 'Euro', 1, '&euro;', 'prefix'),
 (2, 'US Dollar', 0.74, '$', 'prefix'),
-(3, 'SEK', 0.11, 'kr', 'suffix');
+(3, 'Swedish Krona', 0.11, 'SEK', 'suffix'),
+(4, 'BitCoin', 1.59, 'mBTC', 'suffix'),
+(8, 'Norwegian Kroner', 0.12, 'NOK', 'suffix');
 
 -- --------------------------------------------------------
 
@@ -83,19 +88,11 @@ CREATE TABLE IF NOT EXISTS `orders` (
   `ChargedCard` int(11) NOT NULL,
   `OrderIP` text NOT NULL,
   `OrderTotal` int(11) NOT NULL,
+  `ShippingCost` int(11) NOT NULL,
   PRIMARY KEY (`OrderId`),
   KEY `SSNr` (`SSNr`),
   KEY `ChargedCard` (`ChargedCard`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
-
---
--- Dumping data for table `orders`
---
-
-INSERT INTO `orders` (`OrderId`, `SSNr`, `OrderDate`, `Discount`, `ChargedCard`, `OrderIP`, `OrderTotal`) VALUES
-(2, 621955621955, '2014-02-01 14:04:38', 0, 1, 'unknown', 0),
-(3, 621955621955, '2014-02-01 14:06:37', 0, 1, '', 0),
-(4, 621955621955, '2014-02-01 14:07:54', 0, 1, '127.0.0.1', 0);
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=33 ;
 
 -- --------------------------------------------------------
 
@@ -112,21 +109,7 @@ CREATE TABLE IF NOT EXISTS `order_lists` (
   PRIMARY KEY (`OrderListId`),
   KEY `PurchaseId` (`OrderId`),
   KEY `ProductId` (`ProductId`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
-
---
--- Dumping data for table `order_lists`
---
-
-INSERT INTO `order_lists` (`OrderListId`, `OrderId`, `ProductId`, `Amount`, `OrderListTotal`) VALUES
-(1, 2, 9, 2, 0),
-(2, 2, 8, 1, 0),
-(3, 3, 7, 1, 0),
-(4, 3, 8, 2, 0),
-(5, 3, 9, 3, 0),
-(6, 4, 9, 3, 0),
-(7, 4, 8, 2, 0),
-(8, 4, 7, 1, 0);
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=19 ;
 
 -- --------------------------------------------------------
 
@@ -136,7 +119,7 @@ INSERT INTO `order_lists` (`OrderListId`, `OrderId`, `ProductId`, `Amount`, `Ord
 
 CREATE TABLE IF NOT EXISTS `products` (
   `ProductId` int(11) NOT NULL AUTO_INCREMENT,
-  `Name` text NOT NULL,
+  `Name` text CHARACTER SET utf8 COLLATE utf8_swedish_ci NOT NULL,
   `Description` text NOT NULL,
   `ImgUrl` text NOT NULL,
   `Taxanomy` int(11) NOT NULL,
@@ -145,17 +128,43 @@ CREATE TABLE IF NOT EXISTS `products` (
   `ProductWeight` double unsigned NOT NULL,
   PRIMARY KEY (`ProductId`),
   KEY `Taxanomy` (`Taxanomy`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=21 ;
 
 --
 -- Dumping data for table `products`
 --
 
 INSERT INTO `products` (`ProductId`, `Name`, `Description`, `ImgUrl`, `Taxanomy`, `Price`, `Stock`, `ProductWeight`) VALUES
-(7, 'Playstation', 'Sonyï¿½s first console and quite succesful too, runs games  like Crash Bandicoot, Gran Turismo and many more.', '/img/helmet.jpg', 2, 650, 43, 27),
-(8, 'Xbox', 'The Xbox is Microsofts answer to the Playstation, runs the famous game Halo, along with a series of huge titles.', 'Xbox.jpg', 2, 930, 13, 4.5),
-(9, 'GameBoy Color', 'The GameBoy Color is a portable gaming device from Nintendo, most famous for running Pokémon amongst other games. Also has color on screen, hence the name.', 'GameBoyColor.jpg', 2, 366, 71, 0.2),
-(10, 'Nintendo 64', 'Nintendos 64-bit console running games like Super Mario 64, Mario Kart 64 and many more.', 'N64.jpg', 2, 1000, 50, 2);
+(12, 'Jofa LT890', 'Black helmet for all purposes', '/content/img/7_jaws.jpg', 13, 55, 2, 800),
+(13, 'Jofa LT716W', 'White helmet', '/content/img/9_jaws.jpg', 13, 50, 2, 700),
+(16, 'Figure Skates 2000', 'Skates for figure skating', '/content/img/3_jaws.jpg', 17, 800, 2, 1200),
+(18, 'Bauer GH200', 'black hockey helmet', '/content/img/2_jaws.jpg', 19, 90, 10, 1200),
+(19, 'Reebook SK93', 'Hockey skate from Reebook', '/content/img/5_jaws.jpg', 16, 500, 13, 1200),
+(20, 'Bauer NME5 Hockey Goalie', 'Fashionable hockey helmet for goalkeepers', '/content/img/bauer-nme5-hockey-goalie-helmet-VRR-33772-S.jpg', 20, 400, 45, 600);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shipping`
+--
+
+CREATE TABLE IF NOT EXISTS `shipping` (
+  `MaxWeight` int(11) NOT NULL,
+  `Price` int(11) NOT NULL,
+  PRIMARY KEY (`MaxWeight`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `shipping`
+--
+
+INSERT INTO `shipping` (`MaxWeight`, `Price`) VALUES
+(200, 1),
+(500, 2),
+(2000, 5),
+(5000, 8),
+(20000, 25),
+(100000, 50);
 
 -- --------------------------------------------------------
 
@@ -169,17 +178,21 @@ CREATE TABLE IF NOT EXISTS `taxanomies` (
   `TaxanomyParent` int(11) DEFAULT NULL,
   PRIMARY KEY (`TaxanomyId`),
   KEY `TaxanomyParent` (`TaxanomyParent`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=21 ;
 
 --
 -- Dumping data for table `taxanomies`
 --
 
 INSERT INTO `taxanomies` (`TaxanomyId`, `TaxanomyName`, `TaxanomyParent`) VALUES
-(1, 'MasterParent', NULL),
-(2, 'Consoles', NULL),
-(3, 'Games', NULL),
-(4, 'Accessories', NULL);
+(1, 'Root', 0),
+(13, 'Helmets', 1),
+(14, 'Skates', 1),
+(16, 'Hockey Skates', 14),
+(17, 'Figure Skates', 14),
+(18, 'Hockey Helmets', 13),
+(19, 'Cool Hockey Helmets', 18),
+(20, 'Goalie Helmets', 18);
 
 -- --------------------------------------------------------
 
@@ -197,54 +210,23 @@ CREATE TABLE IF NOT EXISTS `users` (
   `PostAddress` text NOT NULL,
   `City` text NOT NULL,
   `Telephone` text NOT NULL,
-  `SessionKey` int(11) NOT NULL,
+  `SessionKey` text NOT NULL,
   `IsAdmin` tinyint(1) DEFAULT NULL,
   `PwSalt` text NOT NULL,
+  `ResetKey` text NOT NULL,
   PRIMARY KEY (`SSNr`),
-  UNIQUE KEY `SSNr` (`SSNr`),
   KEY `SSNr_2` (`SSNr`),
-  FULLTEXT KEY `Mail` (`Mail`)
+  KEY `SSNr_3` (`SSNr`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`SSNr`, `Mail`, `Password`, `FirstName`, `LastName`, `StreetAddress`, `PostAddress`, `City`, `Telephone`, `SessionKey`, `IsAdmin`, `PwSalt`) VALUES
-(1, '2', 'e49e2aed685de5fd1f502e3e528842d2', '4', '5', '6', '7', '8', '9', 0, NULL, '395d05b6fd932b8b91bd3'),
-(12345, 'asd@asd.asd', 'adcebf74e3e24ef7953eee75349cfba5', 'asd', 'asd', 'asd', 'asd', 'asd', 'asd', 0, NULL, '7120'),
-(199205075931, 'marcus@lolmail.com', 'd5b1ee4b463dc7db3b0eaaa0ea2cb5b4', 'Sven', 'Börjesson', 'Tändsticksgränd 11', '55315', 'Jönköping', '0708794290', 0, NULL, '1234'),
-(621955621955, 'marcus@marcus.se', 'bfbe8c51cb64342cead571420e59c8ca', 'marcus', 'lololol', 'Street yo', '12345', 'jonkeeen', '+46123456', 96, NULL, '41afdc2df118e40194453');
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `orders`
---
-ALTER TABLE `orders`
-  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`SSNr`) REFERENCES `users` (`SSNr`),
-  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`ChargedCard`) REFERENCES `cards` (`CardId`);
-
---
--- Constraints for table `order_lists`
---
-ALTER TABLE `order_lists`
-  ADD CONSTRAINT `order_lists_ibfk_1` FOREIGN KEY (`ProductId`) REFERENCES `products` (`ProductId`),
-  ADD CONSTRAINT `order_lists_ibfk_2` FOREIGN KEY (`OrderId`) REFERENCES `orders` (`OrderId`);
-
---
--- Constraints for table `products`
---
-ALTER TABLE `products`
-  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`Taxanomy`) REFERENCES `taxanomies` (`TaxanomyId`);
-
---
--- Constraints for table `taxanomies`
---
-ALTER TABLE `taxanomies`
-  ADD CONSTRAINT `taxanomies_ibfk_1` FOREIGN KEY (`TaxanomyParent`) REFERENCES `taxanomies` (`TaxanomyId`);
+INSERT INTO `users` (`SSNr`, `Mail`, `Password`, `FirstName`, `LastName`, `StreetAddress`, `PostAddress`, `City`, `Telephone`, `SessionKey`, `IsAdmin`, `PwSalt`, `ResetKey`) VALUES
+(1402058120, 'marcusandreas@hotmail.com', '005c9e384f4fd85ad51fbac919d4df2f', 'Marcus', 'BÃ¶rjesson', '10 Downing Street', '99999', 'London', '+463650070', '6bc2593ccc3da79892c44', 1, '749e98fb836ff7343fbf7', ''),
+(9000000000, 'davidklaar@gmail.com', 'f17eddbd49206fba8960db4f2fe848e8', 'David', 'Klar', 'Tre st', 'Jdjfk', 'JÃ¶nkÃ¶ping ', '+4600000', '', 1, '72883c3c58599c64cc5c3', ''),
+(9102011914, 'gustav@glindqvist.se', '1eefdc7a860b2519c3221725b9fe02b5', 'Gustav', 'Lindqvist', 'HermansvÃ¤gen 104', '55453', 'JÃ¶nkÃ¶ping', '46761479126', 'd45e77a7f4464739e42b5', 1, '9ecb0368a92fecee25667', '');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
